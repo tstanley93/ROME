e80f4acc30549927c223e0fb043844608e31f5d6
unknown <stanley@SEA-L-STANLEY2.olympus.F5Net.com>
2015-12-02 09:01:35 -0800

    uploading start.sh

---
+++ start.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+###########################################################################
+##       ffff55555                                                       ##
+##     ffffffff555555                                                    ##
+##   fff      f5    55         Deployment Script Version 0.0.1           ##
+##  ff    fffff     555                                                  ##
+##  ff    fffff f555555                                                  ##
+## fff       f  f5555555             Written By: EIS Consulting          ##
+## f        ff  f5555555                                                 ##
+## fff   ffff       f555             Date Created: 12/02/2015            ##
+## fff    fff5555    555             Last Updated: 12/02/2015            ##
+##  ff    fff 55555  55                                                  ##
+##   f    fff  555   5       This script will start the pre-configured   ##
+##   f    fff       55       WAF configuration.                          ##
+##    ffffffff5555555                                                    ##
+##       fffffff55                                                       ##
+###########################################################################
+###########################################################################
+##                              Change Log                               ##
+###########################################################################
+## Version #     Name       #                    NOTES                   ##
+###########################################################################
+## 11/23/15#  Thomas Stanley#    Created base functionality              ##
+###########################################################################
+
+mv ./blacbox.conf /config/blackbox.conf
+mv ./blackboxstartup.sh /config/blackboxstarup.sh
+/config/blackboxstartup.sh
